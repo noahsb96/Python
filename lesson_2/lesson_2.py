dbe0b608af0a9c28bcfafaf574b9505c19c20bd2
noahsb96 <noahsb96@yahoo.com>
2023-05-04 21:22:36 -0600

    finished lesson 2 of day 2

--- lesson_2/lesson_2.py
+++ lesson_2/lesson_2.py
@@ -18,3 +18,19 @@ print(123 + 345) #This will add mathematically as they are integers
 
 True # Must have first letter capitalized
 False
+
+# len function does not work with integers
+
+# this function will not work and comes up with an error, can only concatenate str (not "int") to str, because we are trying to concatenate an integer
+num_char = len(input("What is your name?"))
+
+new_num_char = str(num_char) # changes integer to string
+
+print("Your name has " + new_num_char + " characters.")
+
+# print(type(num_char)) # checks data type
+
+a = float(123)
+print(70 + float("100.5")) # output is 170.5
+
+print(str(70) + str(100)) # output is 70100
