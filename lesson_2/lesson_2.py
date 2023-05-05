1d4943fe3c0f986c6db5f9a98321245b9d59006d
noahsb96 <noahsb96@yahoo.com>
2023-05-04 21:11:52 -0600

    finished lesson 1 of day 2

---
+++ lesson_2/lesson_2.py
@@ -0,0 +1,20 @@
+# Data Types
+# String
+print("Hello"[4]) # Subscripting
+
+print("123" + "345") # Will not add mathematically like numbers. Will just concatenate to make 12345
+
+# Integer
+
+print(123 + 345) #This will add mathematically as they are integers
+
+123_456_789 # underscores help visualize how big number is without using commas
+
+# Float
+
+3.12159
+
+# Boolean
+
+True # Must have first letter capitalized
+False
