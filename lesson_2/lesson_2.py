5a905b1274bca4ede2a121bf8392e367e27d3dc2
noahsb96 <noahsb96@yahoo.com>
2023-05-11 22:02:25 -0600

    completed lesson 2

--- lesson_2/lesson_2.py
+++ lesson_2/lesson_2.py
@@ -43,16 +43,37 @@
 
 # #PEMDAS
 
-print(round(8 // 3)) # Floor division to create whole number instead of float
+# print(round(8 // 3)) # Floor division to create whole number instead of float
 
-score = 0
+# score = 0
 
-score += 1 # manipulating score to add 1
+# score += 1 # manipulating score to add 1
 
-print(score) 
+# print(score) 
 
-height = 1.8
-isWinning = True
+# height = 1.8
+# isWinning = True
 
-#f-string makes it possible to put all data types in one string
-print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+# #f-string makes it possible to put all data types in one string
+# print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? "))
+people = int(input("How many people to split the bill? "))
+tip_percent = tip / 100
+total_tip = bill * tip_percent
+total_bill = bill + total_tip
+person_pays = total_bill / people
+final_amount = round(person_pays, 2)
+final_amount = "{:.2f}".format(person_pays)
+print(f"Each Person should pay: ${final_amount}")
