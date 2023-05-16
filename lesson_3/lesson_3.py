874010738105f6b433e4b63671bb104fa966e760
noahsb96 <noahsb96@yahoo.com>
2023-05-15 21:10:36 -0600

    finished lesson_3

--- lesson_3/lesson_3.py
+++ lesson_3/lesson_3.py
@@ -1,28 +1,76 @@
-print("Welcome to the rollercoaster!")
-height = int(input("Whata is your height in cm? "))
-bill = 0
+# print("Welcome to the rollercoaster!")
+# height = int(input("Whata is your height in cm? "))
+# bill = 0
 
-if height >= 120:
-    print("You can ride the rollercoaster!")
-    age = int(input("What is your age? "))
-    if age < 12:
-        bill = 5
-        print("Child tickets are $5.")
-    elif age <= 18:
-        bill = 7
-        print("Youth tickets are $7.")
-    elif age >= 45 and age <= 55:
-        bill = 0
-        print("You are going through a midlife crisis. Tickets are free")
-    else:
-        bill = 12
-        print("Adult tickets are $12.")
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         bill = 5
+#         print("Child tickets are $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Youth tickets are $7.")
+#     elif age >= 45 and age <= 55:
+#         bill = 0
+#         print("You are going through a midlife crisis. Tickets are free")
+#     else:
+#         bill = 12
+#         print("Adult tickets are $12.")
 
-    wants_photo = input("Do you want a photo taken? y or n. ")
-    if wants_photo == "y":
-        bill += 3
+#     wants_photo = input("Do you want a photo taken? y or n. ")
+#     if wants_photo == "y":
+#         bill += 3
 
-    print(f"Your final bill is is ${bill}")
+#     print(f"Your final bill is is ${bill}")
 
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+prompt_1 = input("left or right? ")
+step_1 = prompt_1.lower()
+if step_1 == "right":
+    print("Game Over")
 else:
-    print("Sorry, you have to grow taller before you can ride.")
+    prompt_2 = input("Swim or wait? ")
+    step_2 = prompt_2.lower()
+    if step_2 == "swim":
+        print("Game Over.")
+    else:
+        prompt_3 = input("Which door? Red, Blue or Yellow? ")
+        step_3 = prompt_3.lower()
+        if step_3 == "red":
+            print("Game Over.")
+        elif step_3 == "blue":
+            print("Game Over.")
+        else:
+            print("You Win!")
