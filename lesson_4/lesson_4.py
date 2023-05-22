05615d7a1ce64f38b3fe3377600f2f77c50645e1
noahsb96 <noahsb96@yahoo.com>
2023-05-22 16:39:36 -0600

    finished lesson 4 part 4

--- lesson_4/lesson_4.py
+++ lesson_4/lesson_4.py
@@ -13,8 +13,18 @@
 # love_score = random.randint(1, 100)
 # print(f"Your love score is {love_score}")
 
-states_of_america = ["Delaware", "Pennsylvania"]
-states_of_america[1] = "Pencilvania"
-states_of_america.append("Angelaland")
-states_of_america.extend(["Angelaland", "Jack Bauer Land"])
-print(states_of_america)
+# states_of_america = ["Delaware", "Pennsylvania"]
+# states_of_america[1] = "Pencilvania"
+# states_of_america.append("Angelaland")
+# states_of_america.extend(["Angelaland", "Jack Bauer Land"])
+# print(states_of_america)
+
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples",
+#                "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
