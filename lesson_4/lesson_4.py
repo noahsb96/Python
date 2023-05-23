a49d95da5836bdffd86bedf233bd26c7d7e97c0e
noahsb96 <noahsb96@yahoo.com>
2023-05-22 19:57:26 -0600

    lesson 4 complete

--- lesson_4/lesson_4.py
+++ lesson_4/lesson_4.py
@@ -22,9 +22,64 @@
 # dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples",
 #                "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
 
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
-vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
 
-dirty_dozen = [fruits, vegetables]
+# dirty_dozen = [fruits, vegetables]
 
-print(dirty_dozen)
+# print(dirty_dozen[1][2])
+# print(dirty_dozen[1][3])
+
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+# Write your code below this line 👇
+choices = [rock, paper, scissors]
+user = int(input(
+    "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+if user >=3 or user < 0:
+    print("You typed an invalid number. Try again!")
+else:
+    print(choices[user])
+
+    computer = random.randint(0, 2)
+    print("Computer chose: ")
+    print(choices[computer])
+
+
+    if user == 0 and computer == 2:
+        print("You Win!")
+    elif computer == 0 and user == 2:
+        print("You lose!")
+    elif computer > user:
+        print("You Lose!")
+    elif user > computer:
+        print("You win!")
+    elif user == computer:
+        print("No winner. It is a tie!")
