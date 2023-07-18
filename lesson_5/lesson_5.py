d911193a94d090b502350d8896ac0542fe3a1560
noahsb96 <noahsb96@yahoo.com>
2023-07-17 21:00:30 -0600

    completed python lesson 5 part 2

--- lesson_5/lesson_5.py
+++ lesson_5/lesson_5.py
@@ -1,5 +1,13 @@
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " Pie")
-print(fruits)
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " Pie")
+# print(fruits)
+
+# for number in range(1, 11, 3):
+#     print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
