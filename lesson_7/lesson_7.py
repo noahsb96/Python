5b689323a1088c3a3eb10d820b9302cde0aef998
noahsb96 <noahsb96@yahoo.com>
2023-08-14 16:49:03 -0600

    finished lesson 7 part 1

---
+++ lesson_7/lesson_7.py
@@ -0,0 +1,15 @@
+# Step 1
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = word_list[random.randint(0, len(word_list)-1)]
+print(chosen_word)
+
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter! \n").lower()
+print(guess)
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+if guess in chosen_word:
+    print("Rock on!")
