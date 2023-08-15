8be0fc757be99076f655580ae33f2831ee7a20ed
noahsb96 <noahsb96@yahoo.com>
2023-08-14 19:29:22 -0600

    finished lesson 7 part 4

--- lesson_7/lesson_7.py
+++ lesson_7/lesson_7.py
@@ -1,7 +1,67 @@
+#Step 4
+
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+lives = 6
 
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -12,16 +72,26 @@ for _ in range(word_length):
     display.append("_")
 print(display)
 
-# TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-
 while "_" in display:
     guess = input("Guess a letter: ").lower()
     # Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
+            print("That's the cream of the crop, bruthuuuurrr")
             display[position] = letter
-    print(display)
+    if guess not in chosen_word:
+        lives -= 1
+        print("That ain't it bruthuuuurr")
+        if lives == 0:
+            print("You couldn't crack it. I'll see you in the cage, bruthuuuur")
+            break
 
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
     if "_" not in display:
         print("You solved it bruthuuuurrrr")
+
+    print(stages[lives])
